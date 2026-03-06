45d697ec5e14c3dfd4aa33225387a2258ab53304
Lennart Prochnow <lennart.prochnow@cimt-ag.de>
2026-03-06 11:52:32 +0100

    remove unnessary templateproperty annotations

--- src/main/java/io/camunda/connector/model/TMCEndpoint.java
+++ src/main/java/io/camunda/connector/model/TMCEndpoint.java
@@ -1,11 +1,7 @@
 package io.camunda.connector.model;
 
-import io.camunda.connector.generator.java.annotation.TemplateProperty;
-
 public record TMCEndpoint(
-        @TemplateProperty(group = "endpoint", id = "endpoint.entity")
         String entity,
-        @TemplateProperty(group = "endpoint", id = "endpoint.region")
         String region
 ) {
 }
--- src/main/java/io/camunda/connector/model/TMCPayload.java
+++ src/main/java/io/camunda/connector/model/TMCPayload.java
@@ -1,16 +1,12 @@
 package io.camunda.connector.model;
 
-import io.camunda.connector.generator.java.annotation.TemplateProperty;
-
 import java.util.Map;
 
 public record TMCPayload(
-        @TemplateProperty(group = "payload", id = "payload.queryParameters")
         Map<String, Object> queryParameters,
-        @TemplateProperty(group = "payload", id = "payload.body")
         Map<String, Object> body
 ) {
-        public static TMCPayload emptyPayload() {
-                return new TMCPayload(Map.of(), Map.of());
-        }
+    public static TMCPayload emptyPayload() {
+        return new TMCPayload(Map.of(), Map.of());
+    }
 }
